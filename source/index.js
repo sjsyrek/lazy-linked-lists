dfd1b13320d6fdbb47e5df44b951b3470eaab43c
Steven Syrek <steven.syrek@gmail.com>
2016-08-20 14:26:14 -0400

    Create index file

---
+++ source/index.js
@@ -0,0 +1,53 @@
+/**
+ * lazy-linked-lists
+ * Lazy and infinite linked lists for JavaScript.
+ *
+ * index.js
+ *
+ * Top level index.
+ */
+
+export {
+  LT,
+  GT,
+  EQ
+} from './ord';
+
+export {
+  emptyList,
+  list,
+  listRange,
+  listRangeLazy,
+  listRangeLazyBy,
+  listFilter,
+  listAppend,
+  cons,
+  head,
+  last,
+  tail,
+  init,
+  length,
+  isList,
+  isEmpty,
+  fromArrayToList,
+  fromListToArray,
+  fromListToString,
+  fromStringToList,
+  concat,
+  index,
+  filter,
+  map,
+  reverse,
+  sort,
+  sortBy,
+  take,
+  drop,
+  takeWhile,
+  dropWhile,
+  listInf,
+  listInfBy,
+  iterate,
+  repeat,
+  replicate,
+  cycle
+} from './lib';
