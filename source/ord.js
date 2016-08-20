5568a523ecd2d224aaad49bfac568b4c711df295
Steven Syrek <steven.syrek@gmail.com>
2016-08-20 13:07:14 -0400

    Implement orderings as Symbols

---
+++ source/ord.js
@@ -0,0 +1,27 @@
+/**
+ * lazy-linked-lists
+ * Lazy and infinite linked lists for JavaScript.
+ *
+ * ord.js
+ *
+ * Ordering symbols.
+ * @license ISC
+ */
+
+/**
+ * The "less than" ordering. Equivalent to <.
+ * @const {Symbol} LT
+ */
+export const LT = Symbol();
+
+/**
+ * The "greater than" ordering. Equivalent to >.
+ * @const {Symbol} GT
+ */
+export const GT = Symbol();
+
+/**
+ * The "equals" ordering. Equivalent to ===.
+ * @const {Symbol} EQ
+ */
+export const EQ = Symbol();
