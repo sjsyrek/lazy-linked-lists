fe9572dcd37556a4b7bd4cb86a5b86b29f76eed7
Steven Syrek <steven.syrek@gmail.com>
2016-08-22 15:55:56 -0400

    Add example

---
+++ example/example.js
@@ -0,0 +1,40 @@
+/**
+ * lazy-linked-lists
+ * Lazy and infinite linked lists for JavaScript.
+ *
+ * example.js
+ *
+ * Examples of usage for lazy-linked-lists.
+ * @license ISC
+ */
+
+import * as lazy from '../source';
+
+/**
+ * Compute the square root of a number using the Newton-Raphson method.
+ * Adapted from "Why Functional Programming Matters" by John Hughes.
+ * @param {number} a0 - initial estimate
+ * @param {number} eps - tolerance
+ * @param {number} n - number
+ * @returns {number} The square root of `n`
+ * @kind function
+ * @example
+ * sqrt(1,0,2);           // => 1.414213562373095
+ * sqrt(1,0,144);         // => 12
+ * relativeSqrt(1,0,144); // optimized version for very small and very large numbers
+ */
+const sqrt = (a01, eps0, n) => within(eps, lazy.iterate(next.bind(null, n), a0));
+
+const relativeSqrt = (a0, eps, n) => relative(eps, lazy.iterate(next.bind(null, n), a0));
+
+const next = (n, x) => (x + n/x) / 2;
+
+const within = (eps, rest) => {
+  let a = lazy.index(rest, 0), b = lazy.index(rest, 1);
+  return Math.abs(a - b) <= eps ? b : within(eps, lazy.drop(1, rest));
+}
+
+const relative = (eps, rest) => {
+  let a = lazy.index(rest, 0), b = lazy.index(rest, 1);
+  return Math.abs(a - b) <= eps * Math.abs(b) ? b : relative(eps, lazy.drop(1, rest));
+}
