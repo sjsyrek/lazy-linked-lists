14878032f8715d863c42db445087aed9a39fb09e
Steven Syrek <steven.syrek@gmail.com>
2016-08-24 12:02:59 -0400

    Create index file for tests

---
+++ test/index.js
@@ -0,0 +1,13 @@
+/**
+ * lazy-linked-lists
+ * Lazy and infinite linked lists for JavaScript.
+ *
+ * test/index.js
+ *
+ * Top level index for unit tests.
+ * @license ISC
+ */
+
+import 'should';
+
+import './units';
