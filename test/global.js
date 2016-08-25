5dc65ca0a071b77e5f30050e06c3faf44034caa1
Steven Syrek <steven.syrek@gmail.com>
2016-08-25 17:18:21 -0400

    Put test globals into a separate file

---
+++ test/global.js
@@ -0,0 +1,13 @@
+/**
+ * lazy-linked-lists
+ * Lazy and infinite linked lists for JavaScript.
+ *
+ * global.js
+ *
+ * Global scope for unit tests.
+ * @license ISC
+ */
+
+/* global describe, it */
+
+import 'should'
